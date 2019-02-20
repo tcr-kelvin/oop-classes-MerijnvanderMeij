96ef2d02b2bd4f428aafd6385403dc0b072eb8bc
MerijnvanderMeij <merijn.vandermeij@gmail.com>
2019-02-20 16:43:22 +0100

    Opdracht 3.1

--- Opdracht3.php
+++ Opdracht3.php
@@ -4,37 +4,42 @@ echo "Wasmachine: " . $_POST["wasmachine"] . "<br>";
 echo "Droger: " . $_POST["droger"] . "<br>";
 echo "Koelkast: " . $_POST["koelkast"] . "<br>";
 
-class calcdata1 {
+//class calcdata1 {
+//
+//    public $number1;
+//    public $number2;
+//    public $number3;
+//
+//
+//    public function __construct() {
+//        $this->number1 = $number1 = $_POST['wasmachine'];
+//        $this->number2 = $number2 = $_POST['droger'];
+//        $this->number3 = $number3 = $_POST['koelkast'];
+//
+//    }
+//
+//    public function result(){
+//
+//        switch(''){
+//            case "":
+//                return ($this->number1 + $this->number2 + $this->number3);
+//                break;
+//
+//        }
+//    }
+//}
+//$show = new calcdata1();
+//echo "Totaal prijs ex. BTW: " . $show->result() . "<br>";
 
-    public $number1;
-    public $number2;
-    public $number3;
 
-    
-    public function __construct() {
-        $this->number1 = $number1 = $_POST['wasmachine'];
-        $this->number2 = $number2 = $_POST['droger'];
-        $this->number3 = $number3 = $_POST['koelkast'];
-
-    }
-
-    public function result(){
-
-        switch(''){
-            case "":
-                return ($this->number1 + $this->number2 + $this->number3);
-                break;
-
-        }
-    }
-}
-
-$show = new calcdata1();
-echo "Totaal prijs ex. BTW: " . $show->result() . "<br>";
+echo "Totaal prijs ex. BTW: ";
+$som1 = $_POST['wasmachine'] + $_POST['droger'] + $_POST['koelkast'];
+echo $som1;
+echo "<br>";
 
 echo "Totaal prijs incl. BTW: ";
 $som2 = $_POST['wasmachine'] + $_POST['droger'] + $_POST['koelkast'];
-echo $som2 * 1.21;
+echo $som1 * 1.21;
 
 
 
